62605aca1651e8680896ac49642a03ddee257588
Josiah Witt <josiah@jwitt.me>
2019-10-04 21:55:04 -0400

    Add wire encoding functions

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hoistup/hoist-go
+
+go 1.13
+
+require (
+	github.com/JosiahWitt/erk v0.2.0
+	github.com/matryer/is v1.2.0
+)
---
+++ wire/wire.go
@@ -0,0 +1,22 @@
+// Package wire implements the Hoist Wire specification (https://github.com/hoistup/hoist/tree/master/specs/wire.md).
+package wire
+
+import "github.com/JosiahWitt/erk"
+
+// Encoding tracks what Wire encoding is used.
+type Encoding int
+
+// DefaultEncoding is 1, which is the JSON encoding.
+// This may change in the future, if other types are supported.
+const DefaultEncoding = EncodingJSON
+
+// Various encodings
+const (
+	EncodingJSON Encoding = 1
+)
+
+// Error kinds
+type (
+	ErkNilDetails      erk.DefaultKind
+	ErkJSONMarshalling erk.DefaultKind
+)
